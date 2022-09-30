7b19aee43b0fa5f446f166209878cd499f1674b8
tanguangzhi <tanguangzhi@forensix.cn>
2022-09-30 17:54:21 +0800

    feat(): eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended"],
+  extends: ['plugin:react/recommended', 'standard'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react"],
-  rules: {},
-};
+  plugins: ['react'],
+  rules: {
+    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+    'no-unused-vars': 'off',
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['evt'] }],
+    'react/prop-types': ['error', { skipUndeclared: true }],
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'no-sparse-arrays': 1,
+    'no-var': 0,
+    'no-extra-boolean-cast': 1,
+    'no-useless-escape': 1,
+    'linebreak-style': ['off', 'unix', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prefer-const': 0,
+    'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+    'space-before-function-paren': 0
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,53 @@
-import React, { useState, useMemo, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import KanbanBoard, {
-  COLUMN_KEY_DONE,
-  COLUMN_KEY_ONGOING,
-  COLUMN_KEY_TODO,
-} from "./KanbanBoard";
-import AdminContext from "../context/AdminContext";
+import React, { useState, useMemo, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import KanbanBoard, { COLUMN_KEY_DONE, COLUMN_KEY_ONGOING, COLUMN_KEY_TODO } from './KanbanBoard'
+import AdminContext from '../context/AdminContext'
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { title: "开发任务-1", status: "2022-05-22 18:15" },
-    { title: "开发任务-3", status: "2022-06-22 18:15" },
-    { title: "开发任务-5", status: "2022-07-22 18:15" },
-    { title: "测试任务-3", status: "2022-07-23 18:15" },
-  ]);
+    { title: '开发任务-1', status: '2022-05-22 18:15' },
+    { title: '开发任务-3', status: '2022-06-22 18:15' },
+    { title: '开发任务-5', status: '2022-07-22 18:15' },
+    { title: '测试任务-3', status: '2022-07-23 18:15' }
+  ])
   const [ongoingList, setOngoingList] = useState([
-    { title: "开发任务-4", status: "2022-05-22 18:15" },
-    { title: "开发任务-6", status: "2022-06-22 18:15" },
-    { title: "测试任务-2", status: "2022-07-22 18:15" },
-  ]);
+    { title: '开发任务-4', status: '2022-05-22 18:15' },
+    { title: '开发任务-6', status: '2022-06-22 18:15' },
+    { title: '测试任务-2', status: '2022-07-22 18:15' }
+  ])
   const [doneList, setDoneList] = useState([
-    { title: "开发任务-2", status: "2022-06-24 18:15" },
-    { title: "测试任务-1", status: "2022-07-03 18:15" },
-  ]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isAdmin, setIsAdmin] = useState(false);
+    { title: '开发任务-2', status: '2022-06-24 18:15' },
+    { title: '测试任务-1', status: '2022-07-03 18:15' }
+  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isAdmin, setIsAdmin] = useState(false)
   // 对这个写法有疑问，经下面的 a-01 测试理解了, 这样写可以将变量作为 key 去使用。 2022-09-26
   const updaters = {
     [COLUMN_KEY_TODO]: setTodoList,
     [COLUMN_KEY_ONGOING]: setOngoingList,
-    [COLUMN_KEY_DONE]: setDoneList,
-  };
+    [COLUMN_KEY_DONE]: setDoneList
+  }
   const handleAdd = (column, newCard) => {
     // console.log(column, newCard);
-    updaters[column]((currentStat) => [newCard, ...currentStat]);
-  };
+    updaters[column]((currentStat) => [newCard, ...currentStat])
+  }
   const handleRemove = (column, cardToRemove) => {
-    updaters[column]((currentStat) =>
-      currentStat.filter((item) => item.title !== cardToRemove.title)
-    );
-  };
+    updaters[column]((currentStat) => currentStat.filter((item) => item.title !== cardToRemove.title))
+  }
 
   const handleToggleAdmin = (evt) => {
-    setIsAdmin(!isAdmin);
-  };
+    setIsAdmin(!isAdmin)
+  }
 
   useEffect(() => {
     setTimeout(() => {
-      setTodoList(todoList);
-      setOngoingList(ongoingList);
-      setDoneList(doneList);
-      setIsLoading(false);
-    }, 450);
-  }, []);
+      setTodoList(todoList)
+      setOngoingList(ongoingList)
+      setDoneList(doneList)
+      setIsLoading(false)
+    }, 450)
+  }, [])
 
   return (
     <div className="App">
@@ -61,11 +55,7 @@ function App() {
         <h1>
           我的看板
           <label>
-            <input
-              type="checkbox"
-              value={isAdmin}
-              onChange={handleToggleAdmin}
-            />
+            <input type="checkbox" value={isAdmin} onChange={handleToggleAdmin} />
             管理员模式
           </label>
         </h1>
@@ -80,10 +70,10 @@ function App() {
           onAdd={handleAdd}
           onRemove={handleRemove}
           isLoading={isLoading}
-        ></KanbanBoard>
+        />
       </AdminContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
